255ed247f501d9e1235ae0ae40c8db959665aaa9
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-08-09 16:35:27 +0200

    Added clear mode in reboot-devices.py

--- reboot-devices.py
+++ reboot-devices.py
@@ -11,11 +11,21 @@ import dateutil.parser
 import os
 
 parser = ArgumentParser(description="Randomly reboot devices between two points in time.")
-parser.add_argument("address", type=str, help="Network address")
-parser.add_argument("mask", type=str, help="Mask address")
-parser.add_argument("uname", type=str, help="Username on all devices")
-parser.add_argument("passwords", type=str, help="Password list or path to file with password list in it")
-parser.add_argument("time", type=str, help="Time Inverval (START-END)")
+subparsers = parser.add_subparsers(help="sub-command help")
+
+reboot_parser = subparsers.add_parser("reboot", help="Reboot devices in network")
+reboot_parser.add_argument("address", type=str, help="Network address")
+reboot_parser.add_argument("mask", type=str, help="Mask address")
+reboot_parser.add_argument("uname", type=str, help="Username on all devices")
+reboot_parser.add_argument("passwords", type=str, help="Password list or path to file with password list in it")
+reboot_parser.add_argument("time", type=str, help="Time Inverval (START-END)")
+
+undo_parser = subparsers.add_parser("clear", help="Clear all pending reboots")
+undo_parser.add_argument("address", type=str, help="Network address")
+undo_parser.add_argument("mask", type=str, help="Mask address")
+undo_parser.add_argument("uname", type=str, help="Username on all devices")
+undo_parser.add_argument("passwords", type=str, help="Password list or path to file with password list in it")
+
 args = vars(parser.parse_args())
 
 if not os.path.exists(args['passwords']) or not os.path.isfile(args['passwords']):
@@ -26,36 +36,39 @@ else:
 
 uname = args['uname']
 
-start, end = args['time'].split("-")
-start = dateutil.parser.parse(start)
-end = dateutil.parser.parse(end)
+if "time" in args:
+    start, end = args['time'].split("-")
+    start = dateutil.parser.parse(start)
+    end = dateutil.parser.parse(end)
 
-if (start - datetime.datetime.now()).total_seconds() < 0:
-    start += datetime.timedelta(days=1)
-    end += datetime.timedelta(days=1)
+    if (start - datetime.datetime.now()).total_seconds() < 0:
+        start += datetime.timedelta(days=1)
+        end += datetime.timedelta(days=1)
 
 devices = Finder(Address(args['address']), Address(args['mask'])).find_all()
 
 print("Found", len(devices), "devices!")
-print("Generating random minutes...")
+if "time" in args:
+    print("Generating random minutes...")
 
-choosen = []
-minutes = list(range(0, int((end-start).total_seconds()//60)))
+    choosen = []
+    minutes = list(range(0, int((end-start).total_seconds()//60)))
 
-for _ in range(len(devices)):
-    delta = random.choice(minutes)
-    cur_date = start + datetime.timedelta(minutes=delta)
-    choosen.append(cur_date)
-    minutes.remove(delta)
+    for _ in range(len(devices)):
+        delta = random.choice(minutes)
+        cur_date = start + datetime.timedelta(minutes=delta)
+        choosen.append(cur_date)
+        minutes.remove(delta)
 
-print("Random minutes generated!")
+    print("Random minutes generated!")
 
 for i, addr in enumerate(devices):
     if not bool(Port(addr, 22)):
         continue
 
-    exec_time = choosen[i]
-    print(f"{addr} will reboot at {exec_time}")
+    if "time" in args:
+        exec_time = choosen[i]
+        print(f"{addr} will reboot at {exec_time}")
 
     passwd = find_ssh_password(addr, uname, passwds)
     if passwd is None:
@@ -71,9 +84,16 @@ for i, addr in enumerate(devices):
     except AuthenticationException:
         raise ValueError(f"Password `{passwd}` is not valid!")
 
-    target_seconds = int(round((exec_time - datetime.datetime.now()).total_seconds(), 0))
-    print(f"Device will reboot in {target_seconds} seconds")
-    if len(airos.exec(f"sleep {target_seconds} && reboot &")[1]):
-        print("Error occured while executing command!")
+    if "time" in args:
+        target_seconds = int(round((exec_time - datetime.datetime.now()).total_seconds(), 0))
+        print(f"Device will reboot in {target_seconds} seconds")
+        if len(airos.exec(f"sleep {target_seconds} && reboot &")[1]):
+            print("Error occured while executing command!")
+    else:
+        _, err = airos.exec("killall sleep")
+        if err:
+            print("error while executing killall command:", err)
+        else:
+            print(f"Pending reboots cleared for {addr}")
     
 print("All devices done!")
