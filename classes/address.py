8390069449124d916ab67dcbf6d7e621be77c092
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-08-19 11:54:32 +0200

    Moved Address related classes into one file

--- classes/address.py
+++ classes/address.py
@@ -1,11 +1,14 @@
 from typing import Union, TypeVar
 import copy
 import os
+import socket
 
 from .octet import Octet
 from .binary import binary_sum, binary_sub
 
 TAddress = TypeVar("TAddress", bound="Address")
+TOctet = TypeVar("TOctet", bound="Octet")
+
 
 class Address:
     _octets = []
@@ -144,3 +147,147 @@ class Address:
                     new_octets[i-1] -= 1
                     new_octets[i] += 255
         return Address(new_octets)
+
+
+class Port:
+    """
+    Address + Port value. Representing socket with Address and Port values.
+
+    Attributes:
+        addr (Address): Address value of socket
+        value (int): Port value of socket
+    """
+    __slots__ = ["_value", "_addr"]
+    def __init__(self, addr: Address, value: int):
+        self.value = value
+        self.addr = addr
+    
+    @property
+    def value(self) -> int:
+        return self._value
+    
+    @value.setter
+    def value(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError("Port value should be int!")
+        value = int(value)
+        if value not in range(0, 65536):
+            raise ValueError("Port number out of range!")
+        self._value = value
+    
+    @property
+    def addr(self) -> Address:
+        return self._addr
+    
+    @addr.setter
+    def addr(self, value: Address) -> None:
+        if not isinstance(value, Address):
+            raise TypeError("Address should be an `Address` object!")
+        self._addr = value
+    
+    def _check_state(self) -> bool:
+        """
+        Check if this Port object is up in network. (Port is open)
+
+        Returns:
+            bool: Port state (Open/Closed)
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.2)
+        state = sock.connect_ex((str(self._addr), self._value))
+        sock.close()
+        return True if state == 0 else False
+
+    def __int__(self) -> int:
+        return self._value
+    
+    def __bool__(self) -> bool:
+        return True if bool(self._addr) and self._check_state() else False
+    
+    def __str__(self) -> str:
+        return f"{self.addr}:{self.value}"
+
+
+class Octet:
+    _value = 0
+    _ignore_range = False
+    def __init__(self, value: Union[int, str], ignore_range: bool = False) -> None:
+        self._ignore_range = ignore_range
+        self.value = value
+    
+    @property
+    def value(self) -> int:
+        return int(self.value)
+    
+    @value.setter
+    def value(self, value: Union[int, str]) -> None:
+        if type(value) is str and Octet._check_binary(value):
+            value = int(value, 2)
+        else:
+            value = int(value)
+
+        if value not in range(0, 256) and not self._ignore_range:
+            raise ValueError(f"Octet value out of range 0-255! Got: {value}, ignore_range: {self._ignore_range}")
+        self._value = value
+    
+    @staticmethod
+    def _check_binary(value: str) -> bool:
+        return True if len(value) == (value.count("1") + value.count("0")) and len(value) == 8 else False
+
+    def get_binary(self) -> str:
+        return str(format(self._value, "#010b")[2:])
+    
+    def __int__(self) -> int:
+        return int(self._value)
+    
+    def __str__(self) -> str:
+        return str(self._value)
+
+    ### Comparing
+
+    def __gt__(self, other: Union[int, TOctet]) -> bool:
+        if isinstance(other, Octet):
+            return self._value > other._value
+        elif type(other) is int:
+            return self._value > other
+        else:
+            raise TypeError(f"Cannot compare Octet with {type(value)}!")
+    
+    def __lt__(self, other: Union[int, TOctet]) -> bool:
+        if isinstance(other, Octet):
+            return self._value < other._value
+        elif type(other) is int:
+            return self._value < other
+        else:
+            raise TypeError(f"Cannot compare Octet with {type(value)}!")
+
+    def __ge__(self, other: Union[int, TOctet]) -> bool:
+        return self > other or self == other
+    
+    def __le__(self, other: Union[int, TOctet]) -> bool:
+        return self < other or self == other
+    
+    def __eq__(self, other: Union[int, TOctet]) -> bool:
+        if isinstance(other, Octet):
+            return self._value == other._value
+        elif type(other) is int:
+            return self._value == other
+        else:
+            raise TypeError(f"Cannot compare Octet with {type(value)}!")
+    
+    def __ne__(self, other: Union[int, TOctet]) -> bool:
+        return not self == other
+
+    def __add__(self, other: Union[int, TOctet]) -> TOctet:
+        if isinstance(other, Octet):
+            other = int(other)
+        if type(other) is not int:
+            raise TypeError(f"Cannot add value of type `{type(other)}` to octet!")
+        return Octet(int(self) + other, ignore_range=True)
+    
+    def __sub__(self, other: Union[int, TOctet]) -> TOctet:
+        if isinstance(other, Octet):
+            other = int(other)
+        if type(other) is not int:
+            raise TypeError(f"Cannot substract value of type `{type(other)}` from octet!")
+        return Octet(int(self) - other, ignore_range=True)
