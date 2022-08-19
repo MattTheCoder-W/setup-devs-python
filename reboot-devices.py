2db852e11e9c9dbf7b62c05156fccf500271d6cc
MattTheCoder-W <mateusz.wasa@protonmail.com>
2022-08-19 16:27:27 +0200

    Added docstrings

--- reboot-devices.py
+++ reboot-devices.py
@@ -1,22 +1,27 @@
-from classes.finder import Finder
-from classes.address import Address
-from classes.connector import Executor
-from classes.port import Port
-from classes.sshtools import find_ssh_password
+##################################################
+# Automatic Reboot Scheduler for Ubiquiti Devices
+# Author: MattTheCoder-W
+##################################################
 
+import os.path
 import random
-from argparse import ArgumentParser
 import datetime
 import dateutil.parser
-import os
+from argparse import ArgumentParser
 from paramiko.ssh_exception import SSHException, NoValidConnectionsError, AuthenticationException
 
-# Pozyskiwanie argumentów od użytkownika w dwóch trybach:
-# Tryb: reboot (planuje restart urządzeń)
-# Tryb: clear (usuwa zaplanowane restarty urządzeń)
+from classes.finder import Finder
+from classes.address import Address, Port
+from classes.connector import Executor
+from classes.sshtools import find_ssh_password
+
+# Parse user arguments using argaprse in two modes:
+# mode: reboot (plan reboots on devices)
+# mode: clear (remove all planed reboots from devices)
 parser = ArgumentParser(description="Randomly reboot devices between two points in time.")
 subparsers = parser.add_subparsers(help="sub-command help")
 
+# Reboot mode arguments
 reboot_parser = subparsers.add_parser("reboot", help="Reboot devices in network")
 reboot_parser.add_argument("address", type=str, help="Network address")
 reboot_parser.add_argument("mask", type=str, help="Mask address")
@@ -24,6 +29,7 @@ reboot_parser.add_argument("uname", type=str, help="Username on all devices")
 reboot_parser.add_argument("passwords", type=str, help="Password list or path to file with password list in it")
 reboot_parser.add_argument("time", type=str, help="Time Inverval (START-END)")
 
+# Clear mode arguments
 undo_parser = subparsers.add_parser("clear", help="Clear all pending reboots")
 undo_parser.add_argument("address", type=str, help="Network address")
 undo_parser.add_argument("mask", type=str, help="Mask address")
@@ -32,7 +38,7 @@ undo_parser.add_argument("passwords", type=str, help="Password list or path to f
 
 args = vars(parser.parse_args())
 
-# Sprawdzanie czy użytkownik podał ścieżkę do pliku z hasłami czy listę haseł
+# Check if user specified passwords file path or password list
 if not os.path.exists(args['passwords']) or not os.path.isfile(args['passwords']):
     passwds = args['passwords'].split(" ")
 else:
@@ -41,30 +47,28 @@ else:
 
 uname = args['uname']
 
-if "time" in args:  # Jeżeli użytkownik chce ustawić czas ponownego uruchomienia
+if "time" in args:  # "time" is in args dict when user run `reboot` mode
     start, end = args['time'].split("-")
     start = dateutil.parser.parse(start)
     end = dateutil.parser.parse(end)
 
-    # Jeżeli podana godzina już była aktualnego dnia, przenoszona jest na następny dzień.
-    # Np. [O godzinie 12:00] -> 10:00-14:00
-    # Ponowne uruchomienie odbędzie się kolejnego dnia, gdyż 10:00 jest w przeszłości
+    # When specified interval is in the past, move it to next day
     if (start - datetime.datetime.now()).total_seconds() < 0:
         start += datetime.timedelta(days=1)
         end += datetime.timedelta(days=1)
 
-# Pozyskanie listy aktywnych urządzeń w sieci
+# Get all active devices in network
 devices = Finder(Address(args['address']), Address(args['mask'])).find_all()
 
 print("Found", len(devices), "devices!")
-if "time" in args:
+if "time" in args:  # Reboot mode
     print("Generating random minutes...")
 
     choosen = []
-    minutes = list(range(0, int((end-start).total_seconds()//60)))  # Lista możliwych minut
+    minutes = list(range(0, int((end-start).total_seconds()//60)))  # List of possible minutes
 
     for _ in range(len(devices)):
-        # Dla każdego urządzenia losowana jest minuta z listy minut, a następnie dodawana do godziny minimalnej (start)
+        # For every device pick random minute and add it to start time
         delta = random.choice(minutes)
         cur_date = start + datetime.timedelta(minutes=delta)
         choosen.append(cur_date)
@@ -73,38 +77,37 @@ if "time" in args:
     print("Random minutes generated!")
 
 for i, addr in enumerate(devices):
-    if not bool(Port(addr, 22)):
+    if not bool(Port(addr, 22)):  # Check for ssh connection
         continue
 
     if "time" in args:
         exec_time = choosen[i]
         print(f"{addr} will reboot at {exec_time}")
 
-    passwd = find_ssh_password(addr, uname, passwds)  # Znajdywanie poprawnego hasła do urządzenia
+    passwd = find_ssh_password(addr, uname, passwds)  # Function from `classes/sshtools.py`
     if passwd is None:
         print(f"Correct password not found for {addr}!")
         continue
     
     try:
-        # Połączenie z urządzeniem
+        # Connect to device
         airos = Executor(str(addr), 22, uname, passwd)
         print(f"Logged in with uname={uname}, passwd={passwd}")
-    except NoValidConnectionsError:
+    except NoValidConnectionsError:  # SSH was closed
         print(f"{addr} not SSH")
         continue
-    except AuthenticationException:
+    except AuthenticationException:  # Wrong password
         raise ValueError(f"Password `{passwd}` is not valid!")
 
     if "time" in args:
-        # Zamiana wylosowanych minut na sekundy od aktualnej godziny (datetime.now())
+        # Convert picked delay in minutes into seconds
         target_seconds = int(round((exec_time - datetime.datetime.now()).total_seconds(), 0))
         print(f"Device will reboot in {target_seconds} seconds")
-        # Urządzenie będzie czekać w tle x sekund, a następnie się wywoła się reboot
+        # Reboot device after `target_seconds` seconds
         if len(airos.exec(f"sleep {target_seconds} && reboot &")[1]):
             print("Error occured while executing command!")
-    else:
-        # Tryb usuwania zaplanowanych restartów
-        _, err = airos.exec("killall sleep")  # Zabijane są wszystkie procesy "sleep"
+    else:  # Clear mode
+        _, err = airos.exec("killall sleep")  # Kill all sleep processes (scheduled reboots)
         if err:
             print("error while executing killall command:", err)
         else:
